8eb1e8bcd6bd0cc40fe547741b5d4e93d08d2475
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-08-05 06:56:40 +0900

    refactor: dynamically set name of blueprint

--- _app/blueprints/hello.py
+++ _app/blueprints/hello.py
@@ -1,8 +1,8 @@
 from flask import Blueprint
 
-bp_hello = Blueprint("hello", __name__)
+bp = Blueprint(__name__.replace(f"{__package__}.", ""), __name__)
 
 
-@bp_hello.route("/")
+@bp.route("/")
 def hello_world() -> str:
     return "<p>Hello World!</p>"
--- _app/create_app.py
+++ _app/create_app.py
@@ -1,12 +1,16 @@
 from typing import Optional
 
-from _app.blueprints.hello import bp_hello
 from flask import Flask
+
+from blueprints import hello
 
 
 def create_app(app_name: Optional[str] = None) -> Flask:
     if app_name is None:
         app_name = __name__
     app = Flask(app_name)
-    app.register_blueprint(bp_hello)
+
+    # register blueprints
+    app.register_blueprint(hello.bp)
+
     return app
