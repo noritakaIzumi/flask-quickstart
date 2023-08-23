b33b00cae0db4adefc9ba2f3e2a1aa60ffa04857
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-08-24 06:05:24 +0900

    update: ログイン失敗時、フォームの内容を維持する

--- _app/blueprints/login.py
+++ _app/blueprints/login.py
@@ -7,11 +7,19 @@ bp = Blueprint(__name__.replace(f"{__package__}.", ""), __name__)
 
 @bp.route("/login", methods=["GET", "POST"])
 def login() -> str:
+    class Form:
+        def __init__(self, request_form: Optional[Dict[str, str]] = None) -> None:
+            if request_form is None:
+                request_form = {}
+            self.username = request_form.get("username")
+            self.password = request_form.get("password")
+
     error: Optional[str] = None
+    form: Optional[Form] = None
     if request.method == "POST":
-        form: Dict[str, str] = request.form
-        if form.get("username") == "nori" and form.get("password") == "password":
-            return f'Hello, {form.get("username")}!'
+        form = Form(request.form)
+        if form.username == "nori" and form.password == "password":
+            return f"Hello, {form.username}!"
         error = "Invalid username/password"
 
-    return render_template("login.html", action=request.path, error=error)
+    return render_template("login.html", action=request.path, error=error, form=form)
