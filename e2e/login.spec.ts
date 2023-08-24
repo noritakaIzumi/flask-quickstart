4161bfd26c4daa8de31c468d8ccd23cd61aa42d3
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-08-24 22:17:21 +0900

    update: add prettier, eslint, husky, lint-staged + remove .pre-commit-config.yaml

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,42 +1,45 @@
-import {expect, test} from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
-test('get title', async ({page}) => {
-    await page.goto('/login')
+test('get title', async ({ page }) => {
+  await page.goto('/login');
 
-    await expect(page.getByText(/Login form/)).toBeTruthy()
-})
+  await expect(page.getByText(/Login form/)).toBeTruthy();
+});
 
-test('valid login', async ({page}) => {
-    await page.goto('/login')
+test('valid login', async ({ page }) => {
+  await page.goto('/login');
 
-    await page.getByRole('textbox', {name: /username/i}).fill('nori')
-    await page.getByRole('textbox', {name: /password/i}).fill('password')
-    await page.getByRole('button', {name: /submit/i}).click()
+  await page.getByRole('textbox', { name: /username/i }).fill('nori');
+  await page.getByRole('textbox', { name: /password/i }).fill('password');
+  await page.getByRole('button', { name: /submit/i }).click();
 
-    await expect(page.getByText('Hello, nori!')).toBeVisible()
-})
+  await expect(page.getByText('Hello, nori!')).toBeVisible();
+});
 
 test.describe('invalid login', async () => {
-    test.beforeEach(async ({page}) => {
-        await page.goto('/login')
-    })
-    test.afterEach(async ({page}) => {
-        await expect(page.getByText(/Invalid username\/password/)).toBeVisible()
-    })
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/login');
+  });
 
-    test('wrong password', async ({page}) => {
-        await page.getByRole('textbox', {name: /username/i}).fill('nori')
-        await page.getByRole('textbox', {name: /password/i}).fill('password_')
-        await page.getByRole('button', {name: /submit/i}).click()
-    })
+  test('wrong password', async ({ page }) => {
+    await page.getByRole('textbox', { name: /username/i }).fill('nori');
+    await page.getByRole('textbox', { name: /password/i }).fill('password_');
+    await page.getByRole('button', { name: /submit/i }).click();
 
-    test('wrong username', async ({page}) => {
-        await page.getByRole('textbox', {name: /username/i}).fill('nori_')
-        await page.getByRole('textbox', {name: /password/i}).fill('password')
-        await page.getByRole('button', {name: /submit/i}).click()
-    })
+    await expect(page.getByText(/Invalid username\/password/)).toBeVisible();
+  });
 
-    test('input nothing', async ({page}) => {
-        await page.getByRole('button', {name: /submit/i}).click()
-    })
-})
+  test('wrong username', async ({ page }) => {
+    await page.getByRole('textbox', { name: /username/i }).fill('nori_');
+    await page.getByRole('textbox', { name: /password/i }).fill('password');
+    await page.getByRole('button', { name: /submit/i }).click();
+
+    await expect(page.getByText(/Invalid username\/password/)).toBeVisible();
+  });
+
+  test('input nothing', async ({ page }) => {
+    await page.getByRole('button', { name: /submit/i }).click();
+
+    await expect(page.getByText(/Invalid username\/password/)).toBeVisible();
+  });
+});
