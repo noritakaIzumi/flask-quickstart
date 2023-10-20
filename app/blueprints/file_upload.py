c47e24c04412dd9996183de65d731e7ec8016a36
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-10-21 08:53:23 +0900

    fix: Open Redirect

--- app/blueprints/file_upload.py
+++ app/blueprints/file_upload.py
@@ -13,23 +13,23 @@ def upload_file() -> Response | str:
     if request.method == "POST":
         if "file" not in request.files:
             flash("No file part")
-            return redirect(request.url)
+            return redirect(url_for(f".{upload_file.__name__}"))
 
         file = request.files["file"]
         if file.filename is None or file.filename == "":
             flash("No selected file")
-            return redirect(request.url)
+            return redirect(url_for(f".{upload_file.__name__}"))
 
         if not file_is_allowed_to_upload(file.filename):
             flash(f'"{file.filename}" is not allowed to upload')
-            return redirect(request.url)
+            return redirect(url_for(f".{upload_file.__name__}"))
 
         filename = secure_filename(file.filename)
         if filename != file.filename:
             current_app.logger.warning('filename is sanitized to: "%s"', filename)
 
         file.save(os.path.join(current_app.config["UPLOAD_FOLDER"], filename))
-        return redirect(url_for("file_upload.upload_completed"))
+        return redirect(url_for(f".{upload_completed.__name__}"))
 
     return render_template("file_upload.html")
 
