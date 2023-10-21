a4851ae4382fe27bff60d55440308fbec61d248b
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-10-21 09:26:49 +0900

    fix: Use of Hardcoded Credentials

--- tests/blueprints/test_login.py
+++ tests/blueprints/test_login.py
@@ -1,3 +1,6 @@
+import json
+
+from app import repo_root
 from flask.testing import FlaskClient
 from werkzeug.test import TestResponse
 
@@ -10,30 +13,21 @@ class TestLogin:
     def test_login__post__valid_login(self, client: FlaskClient) -> None:
         response: TestResponse = client.post(
             "/login",
-            data={
-                "username": "nori",
-                "password": "password",
-            },
+            data=json.load(open(f"{repo_root}/tests/support/test_login/valid.json")),
         )
         assert "Hello, nori!" in response.text
 
     def test_login__post__invalid_login__wrong_password(self, client: FlaskClient) -> None:
         response: TestResponse = client.post(
             "/login",
-            data={
-                "username": "nori",
-                "password": "password_",
-            },
+            data=json.load(open(f"{repo_root}/tests/support/test_login/wrong_password.json")),
         )
         assert "Invalid username/password" in response.text
 
     def test_login__post__invalid_login__user_not_exists(self, client: FlaskClient) -> None:
         response: TestResponse = client.post(
             "/login",
-            data={
-                "username": "outsider",
-                "password": "password",
-            },
+            data=json.load(open(f"{repo_root}/tests/support/test_login/outsider.json")),
         )
         assert "Invalid username/password" in response.text
 
