50f543842085489553ea675319bc6efd2d8caf91
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-10-28 19:58:49 +0900

    build: fix workers count to 1

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,3 @@
-import multiprocessing
 import os
 from string import Template
 from typing import Literal
@@ -22,7 +21,7 @@ wsgi_app = Template('$app:$fn()').safe_substitute(app=app.__name__, fn=app.creat
 bind = "127.0.0.1:8000"
 
 """worker"""
-workers: int = 1 if mode == 'development' else multiprocessing.cpu_count() * 2 + 1
+workers: int = 1
 worker_class: str = gevent.__name__
 timeout: int = 30 if mode == 'development' else 10
 max_requests: int = 0 if mode == 'development' else 1200
