67ebcde86d575aed60a788fe1ad1779fba9cbd00
Noritaka IZUMI <noritaka.izumi@gmail.com>
2023-12-20 00:06:58 +0000

    update: update devcontainer settings

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -8,20 +8,6 @@ PYTHON_VERSION="3.11.6"
 # update apt packages
 sudo apt update
 
-# install python
-sudo apt install -y \
-    libbz2-dev \
-    libffi-dev \
-    libncurses-dev \
-    libreadline-dev \
-    libsqlite3-dev \
-    tk-dev \
-    python3-tk \
-    liblzma-dev
-asdf plugin add python && \
-    asdf install python $PYTHON_VERSION && \
-    asdf global python $PYTHON_VERSION
-
 # install app
 python -m venv .venv && \
     . .venv/bin/activate
