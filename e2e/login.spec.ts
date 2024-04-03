a6edeac76ce2ccf2f354679090133f47c3c55099
Noritaka IZUMI <noritaka.izumi@gmail.com>
2024-04-03 23:45:56 +0900

    test: refactor tests

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,24 +1,20 @@
 import { expect, test } from '@playwright/test';
 
-test('get title', async ({ page }) => {
-  await page.goto('/login');
-
-  await expect(page.getByText(/Login form/)).toBeTruthy();
-});
-
-test('valid login', async ({ page }) => {
-  await page.goto('/login');
-
-  await page.getByRole('textbox', { name: /username/i }).fill('nori');
-  await page.getByRole('textbox', { name: /password/i }).fill('password');
-  await page.getByRole('button', { name: /submit/i }).click();
-
-  await expect(page.getByText('Hello, nori!')).toBeVisible();
-});
-
 test.describe('invalid login', async () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/login');
+  });
+
+  test('get title', async ({ page }) => {
+    await expect(page.getByText(/Login form/)).toBeTruthy();
+  });
+
+  test('valid login', async ({ page }) => {
+    await page.getByRole('textbox', { name: /username/i }).fill('nori');
+    await page.getByRole('textbox', { name: /password/i }).fill('password');
+    await page.getByRole('button', { name: /submit/i }).click();
+
+    await expect(page.getByText('Hello, nori!')).toBeVisible();
   });
 
   test('wrong password', async ({ page }) => {
